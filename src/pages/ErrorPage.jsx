5a651ba32e3a3c144ae1ca55050b19b24c28e8d3
Eva F <eva.famechon@outlook.com>
2023-06-26 01:08:59 +0200

    Add Home and Error pages

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+
+function ErrorPage() {
+    return (
+        <div className="error404-container">
+            <h1 className="error-title">Erreur 404</h1>
+            <span className="error404-message">
+                Oups! La page que vous demandez n'existe pas.
+            </span>
+            <Link className="error404-home-link" to="/">
+                Retourner sur la page d’accueil
+            </Link>
+        </div>
+    )
+}
+
+export default ErrorPage
