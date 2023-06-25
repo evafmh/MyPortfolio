1c1b48f8ca261b973fb2fec882cb7f4a56c1756e
Eva F <eva.famechon@outlook.com>
2023-06-26 01:09:21 +0200

    add Navigation component

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,21 @@
+import './Navigation.css'
+
+function Navigation(props) {
+    const { aboutText, worksText, contactText } = props
+
+    return (
+        <nav className="navigation-container">
+            <a className="navigation-link" href="#about">
+                {aboutText}
+            </a>
+            <a className="navigation-link" href="#works">
+                {worksText}
+            </a>
+            <a className="navigation-link" href="#contact">
+                {contactText}
+            </a>
+        </nav>
+    )
+}
+
+export default Navigation
