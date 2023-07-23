c4c38a2c4deb95f5daaa7558d0574683f7393d6f
Eva F <eva.famechon@outlook.com>
2023-07-23 18:09:29 +0200

    Remove changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import ErrorPage from './pages/ErrorPage'
 import './sassStyles/main.sass'
